bbcfb1d373d33a29a799695815627815f7414903
Barry Tarlton <btarlton@gmail.com>
2019-06-15 19:20:54 -0400

    Tests for different target scores

--- test_score_keeper.py
+++ test_score_keeper.py
@@ -8,14 +8,28 @@ class ScoreKeeperTest(unittest.TestCase):
 
     def testAddingScoreWhenZeroReturns1(self):
         score_keeper.addScore(1, 'target_1')
-        x = score_keeper.getScoreForTarget('target_1')
-        self.assertEqual(x, 1)
+        scoreForTarget1 = score_keeper.getScoreForTarget('target_1')
+        self.assertEqual(scoreForTarget1, 1)
 
     def testAdding_Score_With_2Hits_To_Same_Target_Returns_2(self):
         score_keeper.addScore(1, 'target_1')
         score_keeper.addScore(1, 'target_1')
-        x = score_keeper.getScoreForTarget('target_1')
-        self.assertEqual(x, 2)
+        scoreForTarget1 = score_keeper.getScoreForTarget('target_1')
+        self.assertEqual(scoreForTarget1, 2)
 
-    def testCanary(self):
-        self.assertTrue(True)
+    def testAdding_1_to_Two_Different_Targets_Returns_1_For_Each(self):
+        score_keeper.addScore(1, 'target_1')
+        score_keeper.addScore(1, 'target_2')
+        scoreForTarget1 = score_keeper.getScoreForTarget('target_1')
+        scoreForTarget2 = score_keeper.getScoreForTarget('target_2')
+        self.assertEqual(scoreForTarget1, 1)
+        self.assertEqual(scoreForTarget2, 1)
+
+    def testAdding_1_to_One_Target_And_2_to_A_Different_Target_Returns_Correct_Totals_For_Each(self):
+        score_keeper.addScore(1, 'target_1')
+        score_keeper.addScore(1, 'target_2')
+        score_keeper.addScore(1, 'target_2')
+        scoreForTarget1 = score_keeper.getScoreForTarget('target_1')
+        scoreForTarget2 = score_keeper.getScoreForTarget('target_2')
+        self.assertEqual(scoreForTarget1, 1)
+        self.assertEqual(scoreForTarget2, 2)
